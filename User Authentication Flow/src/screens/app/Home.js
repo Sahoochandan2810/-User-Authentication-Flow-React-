ddfac2f0a74fac1c8561e50156a00b4f24cb0192
Chandan Kumar Sahoo <163462650+Sahoochandan2810@users.noreply.github.com>
2024-12-12 15:06:50 +0530

    Add files via upload

---
+++ User Authentication Flow/src/screens/app/Home.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import { FlexCentered, Text } from '../../shared/styledComponents';
+
+const Home = () => (
+  <FlexCentered>
+    <Text>Home</Text>
+  </FlexCentered>
+);
+export default Home;
